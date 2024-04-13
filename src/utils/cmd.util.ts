448343dda5f0a6247a0e0d606b226599a2df9498
Panda <499646194@163.com>
2024-04-13 18:35:36 +0800

    wip: cmd

--- src/utils/cmd.util.ts
+++ src/utils/cmd.util.ts
@@ -1,6 +1,5 @@
-import { exec } from 'node:child_process'
-
-import { type SpawnSyncReturns, spawnSync } from 'node:child_process'
+import { ChildProcessWithoutNullStreams, exec } from 'node:child_process'
+import { type SpawnSyncReturns, spawn, spawnSync } from 'node:child_process'
 
 import { Logger } from '@nestjs/common'
 
@@ -19,7 +18,7 @@ export function execute(command: string) {
   })
 }
 
-export function executeSpawn(
+export function executeSpawnSync(
   cmd: string,
   args: string[] = [],
 ): SpawnSyncReturns<string> {
@@ -31,4 +30,39 @@ export function executeSpawn(
     res = result.stdout
 
   return res
+}
+
+// 配置项
+const EXECUTION_OPTIONS = {
+  windowsHide: true, // windows命令行不显示
+  detached: true, // 让子进程独立于父进程运行
+}
+
+let childProcess: ChildProcessWithoutNullStreams
+// 启动服务端程序
+export function executeSpawn(cmd: string, args: string[] = []) {
+  childProcess = spawn(cmd, args, EXECUTION_OPTIONS)
+  console.log('childProcess', childProcess)
+}
+
+// 关闭服务端程序
+export function stopSpawn() {
+  console.log(childProcess.pid)
+  /**
+   * 注意：想要关闭启动的进程，必须使用 spawn 来创建子进程，
+   *      使用 exec 创建子进程无法使用 kill 方法关闭子进程：
+   *      因为 exec 创建子进程的实例实际上不是子进程，而是套壳进程，
+   *      无法获取子进程的 pid，而是在子进程的外部套壳，获取的是套壳进程的 pid
+   */
+  /**
+   * @param 1. childProcess.exitCode 标识子进程的退出代码。如果子进程仍在运行，则返回 null
+   * @param 2. childProcess.kill() 通过调用 kill 方法来杀掉进程
+   */
+  if (childProcess.exitCode === null) {
+    childProcess.kill()
+    console.log('Attempting to gracefully shut down the C++ service...')
+  }
+  else {
+    console.log('C++ service has already exited.')
+  }
 }
